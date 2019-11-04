4be2fd928256271a7a523db45183fc86b3cdbcc3
EYE MOTION <eyemotiongamestudio@gmail.com>
2019-11-04 19:19:39 +0400

    update

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,11 +1,44 @@
 import React from 'react'
+import {MdSend} from 'react-icons/md'
 
 // Arrow Function
-const ExpenseForm = () => {
+const ExpenseForm = ({charge, amount, handleCharge, handleAmount, handleSubmit}) => {
     return (
-        <div>
-                Hello ExpenseForm
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div className="form-center">
+                <div className="form-group">
+                    <label htmlFor="charge">charge</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="charge"
+                        name="charge"
+                        placeholder="e.g. rent" 
+                        value={charge}
+                        onChange={handleCharge}
+                    />
+
+                </div>
+                
+                <div className="form-group">
+                    <label htmlFor="amount">amount</label>
+                    <input 
+                        type="number" 
+                        className="form-control" 
+                        id="amount"
+                        name="amout"
+                        placeholder="e.g. 100" 
+                        value={amount}
+                        onChange={handleAmount}
+                    />
+
+                </div>
+            </div>
+            <button type="submit" className="btn">
+                Submit
+                <MdSend className="btn-icon" />
+            </button>
+        </form>
     )
 }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import {MdEdit, MdDelete} from 'react-icons/md'
 
-const ExpenseItem = () => {
+const ExpenseItem = ({expense}) => {
+    const {id, charge, amount} = expense
+
     return (
-        <div>
-            Hello From Exponse Item
-        </div>
+        <li className="item">
+            <div className="info">
+                <span className="expense">
+                    {charge}
+                </span>
+
+                <span className="amount">
+                    ${amount}
+                </span>
+            </div>
+            <div>
+                <button className="edit-btn" aria-label>
+                    <MdEdit />
+                </button>
+                <button className="clear-btn" aria-label>
+                    <MdDelete />
+                </button>
+            </div>
+        </li>
     )
 }
 
