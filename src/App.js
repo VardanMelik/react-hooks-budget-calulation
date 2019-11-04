bd5b8f73fc774ef99dd3f97a2ebf57d6ed3a4032
EYE MOTION <eyemotiongamestudio@gmail.com>
2019-11-04 18:30:20 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css'
 import ExpenseList from './components/ExpenseList'
 import ExpenseForm from './components/ExpenseForm'
@@ -10,14 +10,25 @@ const initialExpenses = [
   { id: uuid(), charge: "car payment", amount: 400},
   { id: uuid(), charge: "credit card bill", amount: 800}
 ]
-console.log(initialExpenses)
 
 function App() {
+  const [expenses, setExpenses] = useState(initialExpenses)
+ 
   return (
     <>
       <Alert />
-      < ExpenseForm />
-      <ExpenseList />
+      <h1>Budget Calculator</h1>
+      <main className="App">
+        <ExpenseForm />
+        <ExpenseList expenses={expenses}/>
+      </main>
+      <h1>Total Spending: <span className="total">
+        $
+        {expenses.reduce( (acc, curr) => {
+          return (acc += curr.amount)
+        }, 0)}
+        </span></h1>
+
     </>
   );
 }
